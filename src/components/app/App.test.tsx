75e3b0b6cc38daa0fc065784944306a331c03621
AlexandrIvanov <alexandr.1996@list.ru>
2023-10-02 13:23:52 +0300

    Tests for App component

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+    test('render app', () => {
+        render(<App/>);
+        expect(screen.getByRole('list')).toBeInTheDocument();
+        expect(screen.getByText(/find course/i)).toBeInTheDocument();
+    });
+    test('typing in input', () => {
+        render(<App/>)
+        expect(screen.queryByDisplayValue(/react/i)).toBeNull();
+
+        userEvent.type(screen.getByRole('textbox'), 'react');
+
+        expect(screen.getByDisplayValue(/react/i)).toBeInTheDocument();
+    });
+    test('filter works correct', () => {
+        render(<App/>)
+
+        expect(screen.getByText(/css/i)).toBeInTheDocument();
+        expect(screen.getByText(/vue/i)).toBeInTheDocument();
+        expect(screen.getByText(/javascript/i)).toBeInTheDocument();
+
+        userEvent.type(screen.getByRole('textbox'), 'script');
+
+        expect(screen.queryByText(/css/i)).toBeNull();
+        expect(screen.queryByText(/vue/i)).toBeNull();
+        expect(screen.getByText(/javascript/i)).toBeInTheDocument();
+    })
+})
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,20 +1,23 @@
-import React, {useState} from 'react';
-import logo from '../../logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {List} from '../list/List';
 import {Search} from '../search/Search';
 
-export const data = ['js', 'react', 'redux', 'typescript', 'angular']
+export const data = ['javascript', 'react', 'redux', 'typescript', 'angular', 'css', 'vue', 'java']
 
 function App() {
     const [value, setValue] = useState('')
+    const [items, setItems] = useState(data)
+    useEffect(() => {
+        setItems(data.filter(el => el.toLowerCase().includes(value.toLowerCase())))
+    }, [value]);
     return (
         <div className="App">
             Learn react
             <Search value={value} onChange={(e) => setValue(e.currentTarget.value)}>
                 Find course
             </Search>
-            <List items={data}/>
+            <List items={items}/>
         </div>
     );
 }
